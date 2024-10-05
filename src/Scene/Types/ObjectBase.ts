5939cdf11f30238b8f125772919913dc626798ac
Mortvola <mortvola@gmail.com>
2024-10-05 07:57:07 -0600

    Removed ObjectBase.

--- src/Scene/Types/ObjectBase.ts
+++
@@ -1,9 +0,0 @@
-import Entity from "../../State/Entity";
-
-abstract class ObjectBase extends Entity {
-  abstract toDescriptor(): object
-
-  abstract detachSelf(): void
-}
-
-export default ObjectBase;
--- src/Scene/Types/SceneManager.ts
+++ src/Scene/Types/SceneManager.ts
@@ -1,25 +1,24 @@
-import Http from "../../Http/src";
-import type ObjectBase from "./ObjectBase";
+// import Http from "../../Http/src";
 
 class SceneManager {
-  async add(object: ObjectBase) {
-    const descriptor = object.toDescriptor();
+  // async add(object: ObjectBase) {
+  //   const descriptor = object.toDescriptor();
 
-    const response = await Http.post<Omit<unknown, 'id'>, { id: number }>(`/api/scene`, descriptor);
+  //   const response = await Http.post<Omit<unknown, 'id'>, { id: number }>(`/api/scene`, descriptor);
 
-    if (response.ok) {
-      const body = await response.body();
+  //   if (response.ok) {
+  //     const body = await response.body();
 
-      object.id = body.id;
-    }  
-  }
+  //     object.id = body.id;
+  //   }  
+  // }
 
-  async update(object: ObjectBase) {
-    const response = await Http.patch<unknown, void>(`/api/scene/${object.id}`, object.toDescriptor());
+  // async update(object: ObjectBase) {
+  //   const response = await Http.patch<unknown, void>(`/api/scene/${object.id}`, object.toDescriptor());
 
-    if (response.ok) { /* empty */ }  
+  //   if (response.ok) { /* empty */ }  
 
-  }
+  // }
 }
 
 export const sceneManager = new SceneManager();
